f228767a5ff3846cde8dc60d96c805e27a576698
zrvnarmarin <76marin76@gmail.com>
2023-06-21 03:11:32 +0200

    folder organize;home page styling; linkanimations

--- src/app/data/links.tsx
+++ src/app/data/links.tsx
@@ -7,7 +7,7 @@ interface Link {
 export const links: Link[] = [
     { id: 1, value: 'Home', href: '/' },
     { id: 2, value: 'Projects', href: '/projects' },
-    { id: 3, value: 'Blog', href: '/blog' },
+    { id: 3, value: 'Blogs', href: '/blogs' },
     { id: 4, value: 'Resources', href: '/resources' },
 ]
   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import './discoverMoreButton.css'
+import './hover.css'
 
 export default function Home() {
   return (
@@ -24,45 +25,51 @@ export default function Home() {
 
       <div className='mt-80 relative'>
         <div className='text-end pr-24 text-9xl uppercase text-blue-100 font-bold'>Projects</div>
-        <div className='absolute top-8 right-80 text-xl font-bold'>I take pride in delivering high-quality solutions that also leverage my skills as a developer. </div>
+        <div className='absolute top-8 right-80 text-xl font-bold'>I take pride in delivering high-quality solutions to my clients that also leverage my skills as a developer. </div>
         <div className='absolute top-16 right-80 text-xl font-bold'>Every project is a striving to improve. Dive into some of my latest work.</div>
       </div>
 
-        <div className='grid col-span-2 items-center justify-between px-24 mt-72'>
-          <div className='col-start-1 col-end-2'>
-            <div className='text-4xl font-bold mb-32'>01</div>
-            <div className='px-12 text-6xl uppercase font-black mb-16 underline decoration-blue-100 decoration-8 underline-offset-4'>Apartmenify</div>
-            <div className='px-12 text-3xl font-bold mb-4'>Frontend Development</div>
-            <div className='px-12 text-xl font-bold'>This application serves as the app for managing apartments.</div>
-          </div>
+      <div className='grid col-span-2 items-center justify-between px-24 mt-72'>
+        <div className='col-start-1 col-end-2'>
+          <div className='text-4xl font-bold mb-32'>01</div>
+          <div className='px-12 py-8 text-6xl uppercase font-black mb-16 hover:cursor-pointer hover'>Apartmenify</div>
+          <div className='px-12 text-3xl font-bold mb-4'>Frontend Development</div>
+          <div className='px-12 text-xl font-bold'>This application serves as the app for managing apartments.</div>
+        </div>
+      </div>
+
+      <div className='grid col-span-2 items-center justify-between px-24 mt-72'>
+        <div className='col-start-1 col-end-2'>
+          <div className='text-4xl font-bold mb-32'>02</div>
+          <div className='px-12 py-8 text-6xl uppercase font-black mb-16 hover:cursor-pointer hover'>Apartmenify</div>
+          <div className='px-12 text-3xl font-bold mb-4'>Frontend Development</div>
+          <div className='px-12 text-xl font-bold'>This application serves as the app for managing apartments.</div>
+        </div>
+      </div>
+
+      <div className='grid col-span-2 items-center justify-between px-24 mt-72'>
+        <div className='col-start-1 col-end-2'>
+          <div className='text-4xl font-bold mb-32'>03</div>
+          <div className='px-12 py-8 text-6xl uppercase font-black mb-16 hover:cursor-pointer hover'>Apartmenify</div>
+          <div className='px-12 text-3xl font-bold mb-4'>Frontend Development</div>
+          <div className='px-12 text-xl font-bold'>This application serves as the app for managing apartments.</div>
         </div>
 
-        <div className='grid col-span-2 items-center justify-between px-24 mt-72'>
-          <div className='col-start-1 col-end-2'>
-            <div className='text-4xl font-bold mb-32'>02</div>
-            <div className='px-12 text-6xl uppercase font-black mb-16'>Apartmenify</div>
-            <div className='px-12 text-3xl font-bold mb-4'>Frontend Development</div>
-            <div className='px-12 text-xl font-bold'>This application serves as the app for managing apartments.</div>
-          </div>
+      <div className='col-start-2 col-end-3'>
+        {/* <Image src={AuthorImage} alt="gbcfg" /> */}
         </div>
+      </div>
 
-        <div className='grid col-span-2 items-center justify-between px-24 mt-72'>
-          <div className='col-start-1 col-end-2'>
-            <div className='text-4xl font-bold mb-32'>03</div>
-            <div className='px-12 text-6xl uppercase font-black mb-16'>Apartmenify</div>
-            <div className='px-12 text-3xl font-bold mb-4'>Frontend Development</div>
-            <div className='px-12 text-xl font-bold'>This application serves as the app for managing apartments.</div>
-          </div>
+      <div className='text-center mt-36 mb-24'>
+        <Link href="/projects" 
+        className='bg-orange-300 px-24 py-8 text-white font-bold text-2xl discover'>Discover More</Link>
+      </div>
 
-          <div className='col-start-2 col-end-3'>
-            {/* <Image src={AuthorImage} alt="gbcfg" /> */}
-          </div>
-        </div>
-
-        <div className='text-center mt-36 mb-24'>
-          <Link href="/projects" 
-          className='bg-orange-300 px-24 py-8 text-white font-bold text-2xl discover'>Discover More</Link>
-        </div>
+      <div className='mt-80 relative'>
+        <div className='text-start pl-24 text-9xl uppercase text-rose-100 font-bold'>Latest Blogs</div>
+        <div className='absolute top-8 left-80 text-xl font-bold'>Full stack developer who passionately crafts clean, professional and reliable web products.</div>
+        <div className='absolute top-16 left-80 text-xl font-bold'>With my help, you can elevate your digital presence for maximum impact.</div>
+      </div> 
 
     </div>
   )
